73a27bb624dab33b92dcd4a9499b952ed0602472
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-11-06 05:09:21 -0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 {
   const jotDown = document.getElementById("jot-down");
-  const promptText = document.getElementById("form-input-a").value;
-  const descriptionText = document.getElementById("form-input-b").value;
-  const sumbitForm = document.getElementById("submit-form");
-  // const jotPad = document.getElementById("jot-pad");
 
   function openTheForm() {
     if (jotDown.style.display === "none") {
@@ -13,125 +9,116 @@
     }
   }
 
-  // // class Jot {
-  // //   constructor(text) {
-  // //     this.text = text;
-  // //     this.isComplete = false;
-  // //   }
-  // // }
+  if (window.localStorage.getItem("jots") == undefined) {
+    let jots = [];
+    window.localStorage.setItem("jots", JSON.stringify(jots));
+  }
 
-  class JotCards {
-    constructor(selectedHtmlElement) {
-      this.jotCard = JSON.parse(window.localStorage.getItem("jotCards")) || [];
-      this.selectedHtmlElement =
-        selectedHtmlElement || document.getElementById("jot-pad");
-      this.render(this.jotCard);
+  let jots = JSON.parse(window.localStorage.getItem("jots"));
+
+  class Jot {
+    constructor(jotCard) {
+      this.AddCardstoPad(jotCard);
     }
 
-    render(chosenJotArray) {
-      // this.selectedHtmlElement.innerHTML = "";
-      // this.selectedHtmlElement.innerHTML = "";
-      this.addPadWithJotCards(chosenJotArray);
-    }
-
-    addJotCardToPad(promptText, descriptionText) {
-      if (
-        promptText == "" ||
-        promptText == null ||
-        descriptionText == "" ||
-        descriptionText == null
-      ) {
-        alert("Jot something down.");
-      } else {
-        this.jotCard.push(new JotCards(promptText));
-        this.jotCard.push(new JotCards(descriptionText));
-        this.saveJotsInLocalStorage();
-      }
-      this.render(this.jotCard);
-    }
-
-    addPadWithJotCards(chosenJotArray) {
+    AddCardstoPad(jotCard) {
+      //Add jotCard
       const jotCard = document.createElement("div");
       jotCard.className = "jot-card";
-      chosenJotArray.forEach((jotCard, jotCardIndex) => {
-        const cardContent = document.createElement("div");
-        cardContent.classList.add("card-content");
 
-        // add the front of the card
-        const cardFront = document.createElement("div");
-        cardFront.classList.add("card-face", "card-front");
+      // Add card face container
+      const cardContent = document.createElement("div");
+      cardContent.classList.add("card-content");
 
-        const promptContent = document.createElement("p");
-        promptContent.classList.add("prompt-content");
-        const inputA = document.createTextNode(promptText);
-        sumbitForm.addEventListener("click", () => {
-          promptText;
-        });
-
-        // add the back of the card
-        const cardBack = document.createElement("div");
-        cardBack.classList.add("card-face", "card-back");
-
-        const DescriptionContent = document.createElement("p");
-        DescriptionContent.classList.add("description-content");
-        const inputB = document.createTextNode(descriptionText);
-        sumbitForm.addEventListener("click", () => {
-          descriptionText;
-        });
-
-        // add delete card button
-        const deleteButton = document.createElement("button");
-        deleteButton.createTextNode("\u00D7");
-        deleteButton.classList.add("delete-button");
-        deleteButton.setAttribute("aria-label", "Delete the current card");
-        deleteButton.setAttribute("id", "delete-card");
-        deleteButton.addEventListener("click", () => {
-          jotPad.removeChild(jotCard);
-          this.jotCard = this.jotCard
-            .slice(0, jotCardIndex)
-            .concat(this.jotCard.slice(jotCardIndex + 1, this.jotCard.length));
-          this.saveJotsInLocalStorage();
-          this.render(this.jotCard);
-        });
-
-        // add the flip button (flips from front to back)
-        const flipButton = document.createElement("button");
-        flipButton.createTextNode("\u21BA");
-        flipButton.classList.add("flip-button");
-        flipButton.setAttribute(
-          "aria-label",
-          "Flip to the back or front of the card"
-        );
-        flipButton.setAttribute("id", "flip-card");
-        flipButton.addEventListener("click", () => {
-          cardContent.classList.toggle("is-flipped");
-          if (!cardFront) {
-            flipButton.classList.add("display__none");
-            isFrontCard = true;
-          } else {
-            flipButton.classList.remove("display__none");
-            isFrontCard = false;
-          }
-        });
-
-        promptContent.appendChild(inputA);
-        cardFront.appendChild(promptContent);
-        cardFront.appendChild(deleteButton);
-        cardFront.appendChild(flipButton);
-        DescriptionContent.appendChild(inputB);
-        cardBack.appendChild(DescriptionContent);
-        cardFront.appendChild(deleteButton);
-        cardFront.appendChild(flipButton);
-        cardContent.appendChild(cardFront);
-        cardContent.appendChild(cardBack);
+      // Add front of card
+      const cardFront = document.createElement("div");
+      cardFront.classList.add("card-face", "card-front");
+      const promptContent = document.createElement("p");
+      promptContent.classList.add("prompt-content");
+      const sumbitForm = document.getElementById("submit-form");
+      sumbitForm.addEventListener("click", () => {
+        const promptText = document.getElementById("form-input-a").value;
+        if (promptText != "") {
+          promptContent.push(promptText);
+          window.localStorage.setItem("jots", JSON.stringify(jots));
+        }
       });
-      this.selectedHtmlElement.appendChild(jotCard);
+
+      // Add back of card
+      const cardBack = document.createElement("div");
+      cardBack.classList.add("card-face", "card-back");
+      const DescriptionContent = document.createElement("p");
+      DescriptionContent.classList.add("description-content");
+      const sumbitForm = document.getElementById("submit-form");
+      sumbitForm.addEventListener("click", () => {
+        const descriptionText = document.getElementById("form-input-b").value;
+        if (descriptionText != "") {
+          DescriptionContent.push(descriptionText);
+          window.localStorage.setItem("jots", JSON.stringify(jots));
+        }
+      });
+
+      // Add button to delete card
+      const deleteButton = document.createElement("button");
+      deleteButton.createTextNode("\u2425");
+      deleteButton.classList.add("delete-button");
+      deleteButton.setAttribute("aria-label", "Delete the current card");
+      deleteButton.setAttribute("id", "delete-card");
+      deleteButton.addEventListener("click", () => {
+        this.AddCardDelete(jotCard);
+      });
+
+      // Add button to flip card face
+      const flipButton = document.createElement("button");
+      flipButton.createTextNode("\u21BA");
+      flipButton.classList.add("flip-button");
+      flipButton.setAttribute(
+        "aria-label",
+        "Flip to the back or front of the card"
+      );
+      flipButton.setAttribute("id", "flip-card");
+      flipButton.addEventListener("click", () => {
+        this.AddCardFlip;
+      });
+
+      //Append elements to the front of the card
+      promptContent.appendChild(inputA);
+      cardFront.appendChild(promptContent);
+      cardFront.appendChild(deleteButton);
+      cardFront.appendChild(flipButton);
+
+      //Append elements to the back of the card
+      DescriptionContent.appendChild(inputB);
+      cardBack.appendChild(DescriptionContent);
+      cardFront.appendChild(deleteButton);
+      cardFront.appendChild(flipButton);
+
+      //Append all JotCard elements
+      cardContent.appendChild(cardFront);
+      cardContent.appendChild(cardBack);
+      jotCard.appendChild(cardContent);
     }
 
-    saveJotsInLocalStorage() {
-      window.localStorage.setItem("jotCards", JSON.stringify(this.jotCard));
+    AddCardDelete(jotCard) {
+      jotPad.parentNode.removeChild(jotCard);
+      let index = jots.indexOf(jotCard);
+      jots.splice(index, 1);
+      window.localStorage.setItem("jots", JSON.stringify(jots));
+    }
+
+    AddCardFlip() {
+      cardContent.classList.toggle("is-flipped");
+      if (!cardFront) {
+        flipButton.classList.add("display__none");
+        isFrontCard = true;
+      } else {
+        flipButton.classList.remove("display__none");
+        isFrontCard = false;
+      }
     }
   }
 
-  const JotIt = new JotCards();
+  for (let c = 0; c < jots.length; c++) {
+    new Jot(jots[c]);
+  }
 }
